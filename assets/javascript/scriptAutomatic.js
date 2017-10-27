ba87e3c075285817ba8ee90e04219c789ccbb441
SaloniTomar <30462430+SaloniTomar@users.noreply.github.com>
2017-10-27 13:24:40 +0530

    Add files via upload

--- assets/javascript/scriptAutomatic.js
+++ assets/javascript/scriptAutomatic.js
@@ -1,7 +1,9 @@
 //Counter for the current slide
-var slideIndex = 0;
+var slideIndex = 1;
 
 var slideDuration;
+
+var control = "Manual";
 
 /*
 Extract the image locations and respective captions and store this data in corresponding arrays
@@ -13,7 +15,7 @@ xmlhttp.onreadystatechange = function () {
 
         // alert(this.responseText);
         imageData = JSON.parse(this.responseText);
-        
+
     }
 };
 xmlhttp.open("GET", "https://raw.githubusercontent.com/SaloniTomar/slideshow/master/assets/JSON/imageData.JSON", false);
@@ -24,37 +26,25 @@ To display the navigation dots
 */
 var navigationDots = document.getElementById("dotContainer");
 
-    for (var i = 1; i < imageData.length+1; i++) {
-        navigationDots.innerHTML += "<span class='dot' id='dot" + i + "' onclick='slide(" + i + ")'></span> ";
-    }
+for (var i = 1; i < imageData.length + 1; i++) {
+    navigationDots.innerHTML += "<span class='dot' id='dot" + i + "' onclick='slide(" + i + ")'></span> ";
+}
 /*
 To display the images
 */
 function showSlides() {
 
-    //Update the index counter everytime the function is called
-    slideIndex++;
-    
-    //update the value of the time duration of each slide
-    var inputTime = document.getElementById("slideDuration");
-    
-    var previousTime= slideDuration;
-    slideDuration = inputTime.value;
-     
-    if(slideDuration=="" || slideDuration<1){
-        slideDuration= previousTime;
-    }
-    
-    //show slide with current slide index
-    slide(slideIndex);
-    
+     //show slide with current slide index
+        slide(slideIndex);
+
+
     //to automatically play the slides
-    var play = document.getElementById("play");
-    if(play.checked){
+    if (control === "Auto") {
         //Change the image after specified time
+        updateDuration();
+        slideIndex++;
         setTimeout(showSlides, slideDuration * 1000);
-    }
-    else{
+    } else {
         return;
     }
 }
@@ -74,16 +64,45 @@ function slide(n) {
     }
 
     //Set the class of the inactive dots as "dot"
-    for (var i = 1; i < imageData.length+1 ; i++) {
+    for (var i = 1; i < imageData.length + 1; i++) {
         document.getElementById("dot" + i).className = "dot";
     }
 
+    //display the image with the caption
+    image.innerHTML = "<img class='image' src='" + imageData[n - 1].image + "'>";
+    caption.innerHTML = imageData[n - 1].caption;
+    document.getElementById("dot" + n).className = "active";
     //update the slide index
     slideIndex = n;
 
-    //display the image with the caption
-    image.innerHTML = "<img class='image' src='" + imageData[n-1].image + "'>";
-    caption.innerHTML =imageData[n-1].caption;
-    document.getElementById("dot" + n).className = "active";
+}
 
+function updateDuration(){
+    //update the value of the time duration of each slide
+        var inputTime = document.getElementById("slideDuration");
+
+        var previousTime = slideDuration;
+        slideDuration = inputTime.value;
+
+
+        if (slideDuration == "" || slideDuration < 1) {
+            slideDuration = previousTime;
+            inputTime.value = previousTime;
+        }
+}
+
+function setAuto() {
+    control = "Auto";
+    document.getElementById("userInput").style.display = "block";
+    slideIndex++;
+    showSlides();
+    return;
+}
+
+function setManual() {
+    control = "Manual";
+     document.getElementById("userInput").style.display = "none";
+    slideIndex--;
+    showSlides();
+    return;
 }
