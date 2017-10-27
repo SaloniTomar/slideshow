df3a75ffe218ecd7fed44932133820023cd4f74d
SaloniTomar <30462430+SaloniTomar@users.noreply.github.com>
2017-10-27 07:29:51 +0530

    Add files via upload

--- assets/javascript/scriptAutomatic.js
+++ assets/javascript/scriptAutomatic.js
@@ -1,76 +1,90 @@
 //Counter for the current slide
 var slideIndex = 0;
-var input = document.getElementById("slideDuration");
-var slideDuration =2;
-var imageContainer=["./images/img1.jpg", "./images/img2.jpg", "./images/img3.jpg", "./images/img4.jpg", "./images/img5.jpg", "./images/img6.jpg"];
 
+var slideDuration;
 
-function slideshowLoad(){
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var myObj = JSON.parse(this.responseText);
+/*
+Extract the image locations and respective captions and store this data in corresponding arrays
+*/
+var imageContainer = [];
+var imageCaptions = [];
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
 
-            imageContainer = myObj.images;
+        // alert(this.responseText);
+        var myObj = JSON.parse(this.responseText);
+        
+        //alert(myObj.images);
+        imageContainer = myObj.images;
+        
+        //alert(imageContainer.length);
+        imageCaptions = myObj.captions;
 
-        }
+    }
 };
-xmlhttp.open("GET", "https://raw.githubusercontent.com/SaloniTomar/imageSlider/master/assests/JSON/imageData.JSON", true);
+xmlhttp.open("GET", "https://raw.githubusercontent.com/SaloniTomar/slideshow/master/assets/JSON/imageData.JSON", false);
 xmlhttp.send();
 
-}
+/*
+To display the navigation dots
+*/
+var navigationDots = document.getElementById("dotContainer");
 
-var navigationDots= document.getElementById("dotContainer");
-for(var i=1 ; i <= imageContainer.length+1 ; i++){
-    
-    navigationDots.innerHTML= "<span class='dot' id='dot"+ i +"' onclick='slide(" + i +")'></span> ";
-}
-
-
-showSlides();
+    for (var i = 1; i < imageContainer.length+1; i++) {
+        navigationDots.innerHTML += "<span class='dot' id='dot" + i + "' onclick='slide(" + i + ")'></span> ";
+    }
 /*
 To display the images
 */
 function showSlides() {
+
     //Update the index counter everytime the function is called
     slideIndex++;
-    slideDuration =input.value;
-    //alert(""+ slideDuration);
-    slide(slideIndex);
-    //Change the image after specified time
-    setTimeout(showSlides, slideDuration * 1000);
-               
     
+    //update the value of the time duration of each slide
+    var inputTime = document.getElementById("slideDuration");
+    slideDuration = inputTime.value;
+    
+    //show slide with current slide index
+    slide(slideIndex);
+    
+    //to automatically play the slides
+    var play = document.getElementById("play");
+    if(play.checked){
+        //Change the image after specified time
+        setTimeout(showSlides, slideDuration * 1000);
+    }
+    else{
+        return;
+    }
 }
 
 function slide(n) {
-  var image = document.getElementById("image");
-  var caption=document.getElementById("captionText");
-  var arr=["first", "second", "third", "fourth"];
-
+    var image = document.getElementById("image");
+    var caption = document.getElementById("captionText");
 
     //To check whether n is less than the minimum slide index.
-    if(n < 1){
+    if (n < 1) {
         n = imageContainer.length;
-        image.innerHTML="<img class='image' src='"+ imageContainer[n-1]+"'>";
     }
 
     //To check whether n exceeds the total number of images.
-    if(n > imageContainer.length){
-        n=1;
-        image.innerHTML="<img class='image' src='"+ imageContainer[n-1]+"'>";
+    if (n > imageContainer.length) {
+        n = 1;
     }
 
     //Set the class of the inactive dots as "dot"
-    for(var i=1; i< imageContainer.length+1 ; i++){
-        document.getElementById("dot"+i).className= "dot";
+    for (var i = 1; i < imageContainer.length+1 ; i++) {
+        document.getElementById("dot" + i).className = "dot";
     }
+
     //update the slide index
-    slideIndex=n;
+    slideIndex = n;
 
     //display the image with the caption
-    image.innerHTML="<img class='image' src='"+ imageContainer[n-1]+"'>";
-    caption.innerHTML="This is the "+arr[n-1]+" image.";
-    document.getElementById("dot"+n).className = "active";
-    
+    image.innerHTML = "<img class='image' src='" + imageContainer[n - 1] + "'>";
+    caption.innerHTML = imageCaptions[n - 1];
+    document.getElementById("dot" + n).className = "active";
+
 }
